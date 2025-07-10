fd2b32bd31949b3c3ebbd20eef82738371279dea
BridgeRohan <145456953+BridgeRohan@users.noreply.github.com>
2025-07-10 17:10:55 +0530

    been going for hours feel like not moving ahead as faast as i want to, still learning a lot

--- scripts.js
+++ scripts.js
@@ -1,32 +1,48 @@
 const myLibrary = [];
+const showButton = document.querySelector("#dope");
+const closeButton = document.querySelector("#toodope");
+const dialog = document.getElementById('myDialog');
+const form = document.getElementById('myForm');
+showButton.addEventListener("click", function(){
+  dialog.showModal();
+})
+closeButton.addEventListener("click", function(){
+  form.reset();
+  dialog.close();
+})
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = form.elements.title.value.trim();
+  const writer = form.elements.writer.value.trim();
+  const noPage = form.elements.noPage.value;
+  const isRead = form.elements.isRead.checked;
+
+  if (!title || !writer || !noPage) {
+    alert("Please fill in all required fields.");
+    return;
+  }
+  addBookToLibrary(title, writer, noPage, isRead);
+  form.reset();
+  dialog.close();
+  for(let i=0;i<myLibrary.length;i++){
+    const curr=myLibrary[i];
+    console.log(curr.title+curr.writer+curr.noPages+curr.isRead);
+  }
+});
 
 function Book(title, writer, nopages, isRead) {
-  this.id = crypto.randomUUID(); 
+  this.id = crypto.randomUUID();
   this.title = title;
   this.writer = writer;
   this.nopages = nopages;
   this.isRead = isRead;
 }
 
-
-function addBookToLibrary(title,writer,nopages,isRead) {
-  const book = new Book(title,writer,nopages,isRead);
+function addBookToLibrary(title, writer, nopages, isRead) {
+  const book = new Book(title, writer, nopages, isRead);
   myLibrary.push(book);
 }
-/* addBookToLibrary('The Last Wish', 'Andrzej Sapkowski', '288', true);
-addBookToLibrary('Sword of Destiny', 'Andrzej Sapkowski', '384', true);
-addBookToLibrary('Blood of Elves', 'Andrzej Sapkowski', '320', true);
-addBookToLibrary('Time of Contempt', 'Andrzej Sapkowski', '331', true);
-addBookToLibrary('Baptism of Fire', 'Andrzej Sapkowski', '343', true);
-addBookToLibrary('The Tower of the Swallow', 'Andrzej Sapkowski', '436', false);
-addBookToLibrary('The Lady of the Lake', 'Andrzej Sapkowski', '531', false);
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '366', true);
-addBookToLibrary('The Alchemist', 'Paulo Coelho', '182', false);
-addBookToLibrary('On the Road', 'Jack Kerouac', '307', false);
-addBookToLibrary('1984', 'George Orwell', '328', true); */
-addBookToLibrary('The Last Wish', 'Andrzej Sapkowski', '288', true);
-addBookToLibrary('Sword of Destiny', 'Andrzej Sapkowski', '384', true);
-for(let i=0;i<myLibrary.length;i++){
-    const t=myLibrary[i]
-    console.log(t.title+" "+t.writer+" "+t.nopages+" "+t.isRead);
-}
+addBookToLibrary("The Last Wish", "Andrzej Sapkowski", "288", true);
+addBookToLibrary("Sword of Destiny", "Andrzej Sapkowski", "384", true);
+
