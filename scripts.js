f64f08322733d6ee76ba5d4e89721fe8999e87d1
BridgeRohan <145456953+BridgeRohan@users.noreply.github.com>
2025-07-11 01:43:55 +0530

    around 80% done all that is left is styling(i hate this the most)

--- scripts.js
+++ scripts.js
@@ -3,13 +3,15 @@ const showButton = document.querySelector("#dope");
 const closeButton = document.querySelector("#toodope");
 const dialog = document.getElementById('myDialog');
 const form = document.getElementById('myForm');
-showButton.addEventListener("click", function(){
+
+showButton.addEventListener("click", function () {
   dialog.showModal();
-})
-closeButton.addEventListener("click", function(){
+});
+closeButton.addEventListener("click", function () {
   form.reset();
   dialog.close();
-})
+});
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -22,12 +24,14 @@ form.addEventListener('submit', (e) => {
     alert("Please fill in all required fields.");
     return;
   }
+
   addBookToLibrary(title, writer, noPage, isRead);
   form.reset();
   dialog.close();
-  for(let i=0;i<myLibrary.length;i++){
-    const curr=myLibrary[i];
-    console.log(curr.title+curr.writer+curr.noPages+curr.isRead);
+
+  document.getElementById("content").innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    showBooks(myLibrary[i]);
   }
 });
 
@@ -43,6 +47,69 @@ function addBookToLibrary(title, writer, nopages, isRead) {
   const book = new Book(title, writer, nopages, isRead);
   myLibrary.push(book);
 }
+
+function showBooks(curr) {
+  let cont = document.getElementById("content");
+  let inn = document.createElement("div");
+  inn.id = curr.id;
+
+  let tit = document.createElement("h4");
+  tit.id = "tit";
+  tit.innerHTML = curr.title;
+
+  let aut = document.createElement("h6");
+  aut.innerHTML = "by " + curr.writer;
+  aut.id = "aut";
+
+  let nope = document.createElement("h6");
+  nope.innerHTML = "Length: " + curr.nopages;
+  nope.id = "nope";
+
+  let stat = document.createElement("h6");
+  stat.innerHTML = "status: ";
+  stat.id = "stat";
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = 'readCheckbox';
+  checkbox.name = 'isRead';
+  checkbox.checked = curr.isRead === true;
+  const remo = document.createElement('button');
+  remo.className = "remove-btn";
+  remo.innerText = "Remove";
+  remo.setAttribute("data-id", curr.id);
+
+
+  inn.appendChild(tit);
+  inn.appendChild(aut);
+  inn.appendChild(nope);
+  inn.appendChild(stat);
+  inn.appendChild(checkbox);
+  inn.appendChild(remo);
+  cont.appendChild(inn);
+}
 addBookToLibrary("The Last Wish", "Andrzej Sapkowski", "288", true);
 addBookToLibrary("Sword of Destiny", "Andrzej Sapkowski", "384", true);
+for (let i = 0; i < myLibrary.length; i++) {
+  showBooks(myLibrary[i]);
+}
 
+
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.classList.contains("remove-btn")) {
+    const disc = e.target.dataset.id;
+
+    e.target.parentElement.remove();
+
+    for (let i = 0; i < myLibrary.length; i++) {
+      if (disc === myLibrary[i].id) {
+        myLibrary.splice(i, 1);
+        break;
+      }
+    }
+    document.getElementById("content").innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+      showBooks(myLibrary[i]);
+    }
+  }
+});
